e034404c5ea70b5bd7f484b5bf11363313fd0032
Alberto Dubuc <Alberto.Dubuc@alpine.de>
2015-11-18 10:45:06 +0100

    Modification of theTest.c after lost modification on last git experiment and adding CMake preparation

--- theTest.c
+++ theTest.c
@@ -1,8 +1,19 @@
+/* 20151117 by Alendubri */
+/* This is the Test of C file for tutorial on Git */
+#include "theTest.h"
 #include <stdio.h>
 
 int main()
 {
-	printf("Hola, Mundo!\n");
+        int retVal = 0;
 
-	return 0;
+        retVal = printTheHello();
+
+        return retVal;
 }
+
+int printTheHello( void )
+{
+        return  printf("Hola, Mundo!\n");
+}
+
